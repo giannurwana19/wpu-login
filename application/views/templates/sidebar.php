92eba06418d72cc664f273b74080df821f69c2d5
giannurwana19 <giannurwana19@gmail.com>
2020-05-02 16:29:55 +0700

    menambahkan fitur menu management #6

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -12,31 +12,59 @@
       <!-- Divider -->
       <hr class="sidebar-divider">
 
-      <!-- Heading -->
-      <div class="sidebar-heading">
-        Admin
-      </div>
 
-      <!-- Nav Item - Dashboard -->
-      <li class="nav-item">
-        <a class="nav-link" href="index.html">
-          <i class="fas fa-fw fa-tachometer-alt"></i>
-          <span>Dashboard</span></a>
-      </li>
+      <!-- query dari menu, tampilkan menu yang boleh diakses oleh user/admin -->
+      <!-- melakukan join dari ketiga table -->
+      <?php
 
-      <!-- Divider -->
-      <hr class="sidebar-divider">
+      $role_id = $this->session->userdata('role_id');
+      $queryMenu =  "SELECT user_menu.id, menu
+                      FROM user_menu JOIN user_access_menu
+                        ON user_menu.id = user_access_menu.menu_id
+                    WHERE user_access_menu.role_id = $role_id ORDER BY user_access_menu.menu_id ASC";
 
-      <!-- Heading -->
-      <div class="sidebar-heading">
-        User
-      </div>
+      $menu = $this->db->query($queryMenu)->result_array();
+      // var_dump($menu);die;
 
-      <li class="nav-item">
-        <a class="nav-link" href="charts.html">
-          <i class="fas fa-fw fa-user"></i> <!-- fa-fw => fix width -->
-          <span>My Profile</span></a>
-      </li>
+
+      ?>
+
+      <!-- looping menu -->
+      <?php foreach ($menu as $m) : ?>
+        <!-- Heading -->
+        <div class="sidebar-heading">
+          <?= $m['menu']; ?>
+        </div>
+
+        <!-- siapkan submenu sesuai menu -->
+        <?php
+
+        $menu_id = $m['id'];
+        // $querySubMenu = "SELECT *
+        //                   FROM user_sub_menu JOIN user_menu 
+        //                     ON user_sub_menu.id = user_menu.id
+        //                 WHERE user_sub_menu.menu_id = $menu_id
+        //                     AND user_sub_menu.is_active = 1";
+        // ! cara yang tidak pake join
+        $querySubMenu = "SELECT * FROM user_sub_menu 
+                        WHERE menu_id = $menu_id
+                            AND is_active = 1";
+        $subMenu = $this->db->query($querySubMenu)->result_array();
+        ?>
+
+        <?php foreach ($subMenu as $sm) : ?>
+          <!-- Nav Item - Dashboard -->
+          <li class="nav-item">
+            <a class="nav-link" href="<?= base_url($sm['url']) ?>">
+              <i class="<?= $sm['icon'] ?>"></i>
+              <span><?= $sm['title'] ?></span></a>
+          </li>
+        <?php endforeach; ?>
+
+          <hr class="sidebar-divider">
+
+      <?php endforeach; ?>
+
       <li class="nav-item">
         <a class="nav-link" href="<?= base_url('auth/logout') ?>" data-toggle="modal" data-target="#logoutModal">
           <i class="fas fa-fw fa-sign-out-alt"></i> <!-- fa-fw => fix width -->
