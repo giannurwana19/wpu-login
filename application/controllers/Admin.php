46e2694ce38e18c1fdcebbc32ccff43c86a0614c
giannurwana19 <giannurwana19@gmail.com>
2020-05-05 12:38:12 +0700

    menambahkan fitur access management #9

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -15,7 +15,6 @@ class Admin extends CI_Controller{
 
   public function index(){
 
-
     $data['title'] = "Dashboard";
     $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
     // echo "selamat datang " . $data['user']['name'];
@@ -25,5 +24,58 @@ class Admin extends CI_Controller{
     $this->load->view('templates/topbar', $data);
     $this->load->view('admin/index', $data);
     $this->load->view('templates/footer');
+  }
+  public function role(){
+
+    $data['title'] = "Dashboard";
+    $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+
+    $data['role'] = $this->db->get('user_role')->result_array();
+    // echo "selamat datang " . $data['user']['name'];
+
+    $this->load->view('templates/header', $data);
+    $this->load->view('templates/sidebar', $data);
+    $this->load->view('templates/topbar', $data);
+    $this->load->view('admin/role', $data);
+    $this->load->view('templates/footer');
+  }
+
+  public function roleAccess($role_id){
+
+    $data['title'] = "Dashboard";
+    $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+    $data['role'] = $this->db->get_where('user_role', ['id' => $role_id])->row_array();
+    // echo "selamat datang " . $data['user']['name'];
+
+    $this->db->where('id !=', 1);
+    $data['menu'] = $this->db->get('user_menu')->result_array();
+
+    $this->load->view('templates/header', $data);
+    $this->load->view('templates/sidebar', $data);
+    $this->load->view('templates/topbar', $data);
+    $this->load->view('admin/role_access', $data);
+    $this->load->view('templates/footer');
+  }
+
+
+  public function changeAccess(){
+    $menu_id = $this->input->post('menuId'); // diambil dari data menuId pada ajax
+    $role_id = $this->input->post('roleId');
+
+    $data = [
+      'role_id' => $role_id,
+      'menu_id' => $menu_id,
+    ];
+
+    $result = $this->db->get_where('user_access_menu', $data);
+
+    if($result->num_rows() < 1){
+      $this->db->insert('user_access_menu', $data);
+    }else{
+      $this->db->delete('user_access_menu', $data);
+    }
+
+    $this->session->set_flashdata('message', '<div class="alert alert-success text-center" role="alert">Access Changed!</div>');
+
   }
 }
--- application/helpers/wpu_helper.php
+++ application/helpers/wpu_helper.php
@@ -28,3 +28,20 @@ function is_logged_in(){
 
   }
 }
+
+  function check_access($role_id, $menu_id){
+    $ci = get_instance();
+
+    // mencari ke user access menu
+    $ci->db->where('role_id', $role_id);
+    $ci->db->where('menu_id', $menu_id);
+    $result = $ci->db->get('user_access_menu');
+
+    // * contoh query yang lain selain query diatas
+    // $ci->db->get_where('user_access_menu', ['role_id'=> $role_id, 'menu_id'=> $menu_id]);
+
+    if($result->num_rows() > 0){
+      return 'checked="checked"';
+    }
+
+  }
